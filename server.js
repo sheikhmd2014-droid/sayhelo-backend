4ba6d7386145d2fcf19b08ae4a81a1b45c1295b4
sheikhmd2014-droid <sheikhmd2014@gmail.com>
2026-01-09 01:28:24 +0530

    Fix console log message format in server.js

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ io.on("connection", (socket) => {
 // Server start
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(✅ Server running on port ${PORT});
+ console.log(Server running on port ${PORT});
 });
